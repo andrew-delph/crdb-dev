6768aadb780a756c47fe218f262522ce6ee59851
Andrew Delph <andrewdelph@live.ca>
2024-11-04 15:37:53 -0800

    add test identity to script

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func main() {
 	conn := cockroachConn()
 	defer conn.Close(context.Background())
 
-	test(conn)
+	// test(conn)
+	testIdentity(conn)
 
 }
 
@@ -58,6 +59,52 @@ func test(conn *pgx.Conn) {
 
 		fmt.Printf("Sequence value: %d\n", seqValue)
 	}
+}
+
+func testIdentity(conn *pgx.Conn) {
+
+	drop := `
+		DROP TABLE IF EXISTS test_identity;
+		`
+
+	// Execute the query to create the table
+	_, err = conn.Exec(context.Background(), drop)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	create := `
+		CREATE TABLE test_identity (id bigint GENERATED ALWAYS AS IDENTITY (MAXVALUE 10), temp int);
+		`
+
+	// Execute the query to create the table
+	_, err = conn.Exec(context.Background(), create)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 4; i++ {
+		_, err = conn.Exec(context.Background(), `INSERT INTO test_identity (temp) VALUES (1)`)
+		if err != nil {
+			log.Fatalf("Unable to insert row: %v\n", err)
+		}
+	}
+
+	rows, err := conn.Query(context.Background(), `SELECT id FROM test_identity`)
+	if err != nil {
+		log.Fatalf("Unable to retrieve rows: %v\n", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			log.Fatalf("Unable to scan row: %v\n", err)
+		}
+		fmt.Printf("ID: %d\n", id)
+	}
+
 }
 
 func postgresConn() *pgx.Conn {
